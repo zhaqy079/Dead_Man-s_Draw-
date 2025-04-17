02ed53cbd54f33f7002630b2b281550a4a3169d3
zhaqy079 <zhaqy079@mymail.unisa.edu.au>
2025-04-17 23:54:05 +0930

    Implement Card header file 1. Constructor with initialiser list and implement an virtual destructor 2. Implement processor because arributes value and cardType are protected

--- Dead_Man’s_Draw++/Card.h
+++ Dead_Man’s_Draw++/Card.h
@@ -1,7 +1,8 @@
 #ifndef CARD_H
 #define CARD_H
 
-# include <string>
+#include <string>
+#include <vector>
 
 // Define for use as paramter
 class Game;
@@ -9,9 +10,16 @@ class Player;
 
 class Card {
 public:
-    Card(); // Default Constructor.
-    enum CardType {Cannon, Chest, Key, Anchor, Sword, Hook,Oracle, Map, Mermaid, Kraken};// Define a enum for the type of card
-    const CardType& type() const; // Return card type 
+    enum CardType { Cannon, Chest, Key, Anchor, Sword, Hook, Oracle, Map, Mermaid, Kraken };// Define a enum for the type of card
+
+    Card(int v, CardType t)
+        : value(v), cardType(t) {
+    }; // Constructor with initialiser list.
+    virtual ~Card() = default;
+
+    virtual const CardType& type() const { return cardType; }; // Return card type 
+    int getValue() const { return value; };
+
     virtual std::string str() const = 0; // Return card type and its value as string
     virtual void play(Game& game, Player& player) = 0; // Help player play the card ( add or remove) 
     virtual void willAddToBank(Game& game, Player& player) {}; // help when player bank the card
@@ -20,9 +28,11 @@ protected:
     int value; 
     CardType cardType;
 
-
+// For easier using std::vector<Card*> 
+    typedef std::vector<Card*> CardCollection;
 };
 #endif
 
 // Reference: https://learn.microsoft.com/en-us/cpp/cpp/abstract-classes-cp
 // https://github.com/extraPaul/CardGame/blob/master/CardGameProject/Card.h
+// https://www.geeksforgeeks.org/typedef-in-cpp/
