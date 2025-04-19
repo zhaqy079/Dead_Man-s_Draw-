1596b3c109ea524d3888b604238543dfe791623b
zhaqy079 <zhaqy079@mymail.unisa.edu.au>
2025-04-19 23:28:35 +0930

    Implement playCard() feature 1. Do a card type bust check, return true if two cards have same type. 2. Trigger card ability using current Game and Player and function return false.

--- Dead_Man’s_Draw++/Player.cpp
+++ Dead_Man’s_Draw++/Player.cpp
@@ -12,3 +12,19 @@ Player::Player() {
 std::string Player::getName() const {
     return name;
 }
+
+// Implement playCard function adds a card to the play area and remove it from the deck 
+// Reference: https://en.cppreference.com/w/cpp/language/range-for
+bool Player::playCard(Card* card, Game& game) {
+    for (Card* c : playArea) { // Loop through each card pointer in the playArea vector
+        if (c->type() == card->type()) { // Compare exist card and new get card type
+            std::cout << "BUST!" << name << "loses all cards in play area." << std::endl;
+            return true; // Return true when causes bust
+        }
+    }
+    // Otherwise performed card ability, added to playArea, and return false 
+    playArea.push_back(card);
+    card->play(game, *this); // pass current game and player object reference to achieve related card ability
+    return false;
+
+}
