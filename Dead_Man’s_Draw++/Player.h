5dd13cacff459d1473d2627c3d85aa53a898af2d
zhaqy079 <zhaqy079@mymail.unisa.edu.au>
2025-04-19 16:32:46 +0930

    Implement Player header file methods Follow the UML design, implemnt playCard(), bankCard(), getScore(), calculateScore(), printPlayArea() and printBank methods.

--- Dead_Man’s_Draw++/Player.h
+++ Dead_Man’s_Draw++/Player.h
@@ -4,11 +4,22 @@
 #include <string>
 #include "Card.h"
 
+// Define for use as parameter
+class Game;
+
+
 class Player {
 public: 
     Player(); // Default constructor
-    std::string getName() const; // const getter method for access private attribute name 
 
+    std::string getName() const; // const getter method for access private attribute name 
+    bool playCard(Card* card, Game& game); // Player's play area for adding or removing cards
+    void bankCard();
+    int getScore() const;
+    int calculateScore() const;
+    void printPlayArea() const;
+    void printBank() const;
+    
 private:
     std::string name;
     CardCollection playArea; // using typedef at Card.h
