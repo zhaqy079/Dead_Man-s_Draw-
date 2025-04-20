03914c20780805582bec04ddd26603f8bf99f1f4
zhaqy079 <zhaqy079@mymail.unisa.edu.au>
2025-04-20 18:28:38 +0930

    Place shuffleDeck() function and include relavant library

--- Dead_Man’s_Draw++/Game.cpp
+++ Dead_Man’s_Draw++/Game.cpp
@@ -1,1 +1,12 @@
 #include <iostream>
+#include "Card.h"
+#include <algorithm>
+#include <random>
+
+// Place the shuffleDeck function from introduction 
+
+void shuffleDeck(CardCollection & cards) { 
+    CardCollection shuffleDeck{ cards.begin(), cards.end() }; 
+    std::shuffle(shuffleDeck.begin(), shuffleDeck.end(), std::mt19937{std::random_device{}() });
+    std::copy(shuffleDeck.begin(), shuffleDeck.end(), cards.begin());
+}
