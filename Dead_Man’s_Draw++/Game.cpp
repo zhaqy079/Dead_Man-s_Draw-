2123f76bf0ba412e6bf9e2aef8126e07c2ba740a
zhaqy079 <zhaqy079@mymail.unisa.edu.au>
2025-04-20 23:14:38 +0930

    Create a pointer for drawn Card, which point to the last card in the CardCollocetion, and check if its busted, if not, display the play area

--- Dead_Man’s_Draw++/Game.cpp
+++ Dead_Man’s_Draw++/Game.cpp
@@ -19,29 +19,38 @@ void Game::takeTurn() {
     std::cout << currentPlayer->getName() << "'s Bank: " << std::endl;
     currentPlayer->printBank();
     std::cout << "| Score: " << currentPlayer->getScore() << std::endl;
-    
-    
 
-    // Game continus 
-    std::string choice;
-    std::cout << "Draw again? (y/n): ";
-    std::cin >> choice;
+    // Create a pointer for drawn Card, which point to the last card in the CardCollocetion
+    // Reference: https://stackoverflow.com/questions/36748657/using-pop-back-within-a-public-method-but-once-i-exit-that-method-the-vector
+    Card* drawnACard = deck.back();
+    deck.pop_back();
 
-    if (choice == "y") {
-        return;
+    std::cout << currentPlayer->getName() << " draws a " << drawnACard->str() << std::endl;
+
+    bool checkBusted = currentPlayer->playCard(drawnACard, *this);
+
+    if (!checkBusted) {
+        std::cout << currentPlayer->getName() << "'s Play Area: " << std::endl;
+        currentPlayer->printPlayArea();
+
+        // Game continus 
+        std::string choice;
+        std::cout << "Draw again? (y/n): ";
+        std::cin >> choice;
+
+        if (choice == "y") {
+            return;
+        }
+        else {
+            currentPlayer->bankCard();  // Move play area to bank
+        }
+
+        // Switch player after turn ends
+        // Reference: https://stackoverflow.com/questions/12737637/swap-a-pointer-between-two-objects
+        currentPlayer = (currentPlayer == &player1) ? &player2 : &player1;
+        count++;
+
     }
-    else {
-        currentPlayer->bankCard();  // Move play area to bank
-    }
-
-    // Switch player after turn ends
-    // Reference: https://stackoverflow.com/questions/12737637/swap-a-pointer-between-two-objects
-    currentPlayer = (currentPlayer == &player1) ? &player2 : &player1;
-    count++;
-
-}
-// isBust() detect two card is same or not, if busted, discard all card 
-bool Game::isBust() {
 }
 // end game show final score 
 void Game::endGame() {
