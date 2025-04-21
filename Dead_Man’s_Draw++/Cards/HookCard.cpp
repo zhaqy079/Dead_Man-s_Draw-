f223f28b8aca1e0a4f267abcdc0a3975f3ce2a4b
zhaqy079 <zhaqy079@mymail.unisa.edu.au>
2025-04-21 19:23:55 +0930

    For HookCard , add empty bank feature, if no card in bank, back to next turn. For oracleCard. cpp rename the file name.

--- Dead_Man’s_Draw++/Cards/HookCard.cpp
+++ Dead_Man’s_Draw++/Cards/HookCard.cpp
@@ -1,3 +1,18 @@
 #include "HookCard.h"
 #include "../Game.h"
+#include "../Player.h"
 #include <iostream>
+
+void HookCard::play(Game& game, Player& player) {
+    // Access current bank
+    CardCollection& bank = player.getBank();
+
+    // Add empty detection: 
+    if (bank.empty()) {
+        std::cout << "No cards in your Bank. Play continues" << std::endl;
+        return;
+    }
+
+    // Using for-loop find highest value 
+  
+}
