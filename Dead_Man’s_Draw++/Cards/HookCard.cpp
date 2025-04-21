dac3bf49d0f870d5893b24043d62fb4f0082c40d
zhaqy079 <zhaqy079@mymail.unisa.edu.au>
2025-04-21 19:33:32 +0930

    Implement find highest each suit of card method create a 10 eleement array, and 10 card pointer, using loop bank find the highest value card of each suit.

--- Dead_Man’s_Draw++/Cards/HookCard.cpp
+++ Dead_Man’s_Draw++/Cards/HookCard.cpp
@@ -14,5 +14,16 @@ void HookCard::play(Game& game, Player& player) {
     }
 
     // Using for-loop find highest value 
-  
+    int groupMax[10] = { 0 }; // Total 10 types of card, all initialise to 0 element array, and only one max for each card type
+    Card* topCard[10] = { nullptr };
+    // For each card in the bank, check if its the highest of the card type
+    for (Card* card : bank) {
+        int index = static_cast<int>(card->type());
+        int value = card->getValue();
+
+        if (value > groupMax[index]) {
+            groupMax[index] = value;
+            topCard[index] = card;
+        }
+    }
 }
